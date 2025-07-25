9bfc1699ecfeec1d744a4406190c846c453575cd
Endgineer <bm.alkhudairy@hotmail.com>
2025-07-25 02:09:01 -0400

    Disabled curse ticking on non-survival mode.

--- src/main/java/net/endgineer/curseoftheabyss/core/ModEvents.java
+++ src/main/java/net/endgineer/curseoftheabyss/core/ModEvents.java
@@ -72,7 +72,7 @@ public class ModEvents {
 
     @SubscribeEvent
     public static void onPlayerTick(TickEvent.PlayerTickEvent event) {
-        if(event.side == LogicalSide.SERVER && event.phase.equals(TickEvent.Phase.END)) {
+        if(event.side == LogicalSide.SERVER && event.phase.equals(TickEvent.Phase.END) && event.player.gameMode.isSurvival()) {
             event.player.getCapability(CurseProvider.CURSE).ifPresent(curse -> {
                 curse.tick(event.player);
 
